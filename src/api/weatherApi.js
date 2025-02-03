496422cdd53b70259750bbd24b34c522210846f3
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-03 14:38:10 -0600

    changes status to https

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,5 +1,5 @@
 const API_KEY = '2713093eb1af4980b37203809252801';
-const BASE_URL = 'http://api.weatherapi.com/v1/current.json';
+const BASE_URL = 'https://api.weatherapi.com/v1/current.json';
 
 export const fetchWeatherData = async (location) => {
     try {
